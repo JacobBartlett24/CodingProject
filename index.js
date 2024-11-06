daca881f25d797ca56a8735aa9f5ce0b2925c4f4
Jacob Bartlett <96956475+JacobBartlett24@users.noreply.github.com>
2024-11-06 23:24:11 +0000

    Add base methods

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const readline = require("node:readline");
-const fs = require("node:fs");
-const path = require("node:path");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+let directory = [
+  "foo",
+  "bar",
+  "foo/foo1",
+  "bar/bar1",
+  "foo/foo1/foo2",
+  "bar/bar1/bar2",
+];
 
 rl.on("line", (input) => {
   if (input.toLowerCase() === "exit") {
@@ -19,53 +26,127 @@ rl.on("line", (input) => {
 
 function selectMethod(inputLine) {
   if (inputLine[0].toUpperCase() == "CREATE") {
-    createFile(inputLine);
+    createFolder(inputLine);
   } else if (inputLine[0].toUpperCase() == "LIST") {
-    listFiles(".");
+    listFolders(".", 0);
+  } else if (inputLine[0].toUpperCase() == "DELETE") {
+    deleteFolder(inputLine);
+  } else if (inputLine[0].toUpperCase() == "MOVE") {
+    moveFolder(inputLine);
   }
 }
 
-function listFiles(fullPath) {
-  const fileDepthCount = fullPath.split("/").length - 1;
-  let indention = "";
-  for (let i = 0; i < fileDepthCount; i++) {
-    indention += "ss";
-  }
+function moveFolder(inputLine) {
+  const fromFolder = inputLine[1];
+  const toFolder = inputLine[2];
 
-  fs.readdir(fullPath, (err, files) => {
-    if (err) {
-      console.error("Error reading directory:", err);
-      return;
-    }
+  const fromFolderIndex = directory.indexOf(fromFolder);
 
-    const folders = files.filter((file) =>
-      fs.statSync(path.join(fullPath, file)).isDirectory()
-    );
-
-    if (folders.length > 0) {
-      folders.forEach((folder) => {
-        if (folder !== ".git") {
-          listFiles(path.join(fullPath, folder));
-          console.log(`${indention}${folder}`);
-        }
-      });
-    }
-  });
-}
-
-function createFile(inputLine) {
-  folderName = inputLine[1];
-
-  if (inputLine.length > 2) {
-    console.warn("Too many args for 'CREATE'");
+  if (fromFolderIndex === -1) {
+    console.log(`Could not move`);
     return;
   }
 
-  try {
-    if (!fs.existsSync(folderName)) {
-      fs.mkdirSync(folderName);
-    }
-  } catch (err) {
-    console.warn("Cannot create folder");
+  if (!directory.includes(toFolder)) {
+    console.log(`Could not move`);
+    return;
   }
+
+  let slicePoint = fromFolder.split("/").length - 1;
+
+  directory = directory.map((folder) => {
+    if (folder.includes(fromFolder)) {
+      let splitFolder = folder.split("/");
+      let newPath = splitFolder.slice(slicePoint);
+      return `${toFolder}/${newPath.join("/")}`;
+    }
+    return folder;
+  });
+}
+
+function listFolders() {
+  const sortedDirectory = sortDirectory();
+
+  for (let i = 0; i < sortedDirectory.length; i++) {
+    let splitString = sortedDirectory[i].split("/");
+    let indentation = "";
+    splitString.forEach((section, i) => {
+      if (i !== 0) {
+        indentation += "  ";
+      }
+    });
+
+    console.log(`${indentation}${splitString[splitString.length - 1]}`);
+  }
+}
+
+function sortDirectory() {
+  return directory.sort((a, b) => {
+    const aParts = a.split("/");
+    const bParts = b.split("/");
+
+    for (let i = 0; i < Math.min(aParts.length, bParts.length); i++) {
+      if (aParts[i] !== bParts[i]) {
+        return aParts[i].localeCompare(bParts[i]);
+      }
+    }
+
+    return aParts.length - bParts.length;
+  });
+}
+
+function createFolder(inputLine) {
+  let hasError = createCommandHasErrors(inputLine);
+
+  if (hasError) {
+    return;
+  }
+
+  folderName = inputLine[1];
+
+  if (folderName.indexOf("/") > -1) {
+    let folders = folderName.split("/");
+    let path = folders[0];
+
+    for (let i = 0; i < folders.length - 1; i++) {
+      if (i !== 0) {
+        path += `/${folders[i]}`;
+      }
+
+      if (!directory.includes(path)) {
+        console.warn(
+          `Cannot create ${folderName}, ${folders[i]} does not exist`
+        );
+        return;
+      }
+    }
+  }
+
+  directory.push(folderName);
+}
+
+function createCommandHasErrors(inputLine) {
+  if (inputLine.length > 2) {
+    console.warn("Too many args for 'CREATE'");
+    return true;
+  } else if (inputLine.length == 1) {
+    console.warn("Too few args for 'CREATE'");
+    return true;
+  }
+  return false;
+}
+
+function deleteFolder(inputLine) {
+  folderName = inputLine[1];
+
+  var folderIndex = directory.indexOf(folderName);
+
+  if (folderIndex == -1) {
+    console.warn(`Cannot remove ${folderName}, does not exist.`);
+    return;
+  }
+
+  directory = directory.filter((folder) => {
+    return !folder.includes(folderName);
+  });
 }
